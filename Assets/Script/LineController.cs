9ebad697440350caa99e0fec67258ab60fc0b5fe
RyutoOhmori <ryutoomori0414@gmail.com>
2022-11-14 23:28:45 +0900

    [add] テキストを管理するクラスを追加

--- Assets/Script/LineController.cs
+++ Assets/Script/LineController.cs
@@ -2,13 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class LineController : MonoBehaviour
 {
-    [Header("読み込むテキスト")]
-    [Tooltip("シナリオのテキスト"), SerializeField]
-    TextAsset _scenarioText;
+    class Linedata
+    { 
+        string _state;
+        string _name;
+        string _line;
 
+        string State { get => _state; }
+        string Name { get => _name; }
+        string Line { get => _line; }
+
+        Linedata(string line = "", string name = "", string state = "")
+        {
+            _name = name;
+            _state = state;
+            _line = line;
+        }
+    }
+
+    List<Linedata> _linedata = new List<Linedata>();
     /// <summary>現在の行</summary>
     int _lineNum = default;
 
@@ -22,7 +38,11 @@ public class LineController : MonoBehaviour
 
     private void Start()
     {
-        Array.ForEach(_scenarioText.text.Split(), i => _line.Add(i));
+        string introductionPath = Application.persistentDataPath + "/introduction.txt";
+        var introductionText = File.ReadAllText(introductionPath).Split('|');
+
+        foreach (var n in introductionText)
+
     }
 
     /// <summary>
