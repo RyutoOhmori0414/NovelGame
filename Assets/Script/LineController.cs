5642614f69728d45c55ef88537f4e0d4052bd948
RyutoOhmori <ryutoomori0414@gmail.com>
2022-11-20 21:38:12 +0900

    [add] シーンを読み取る枠組みを作った

--- Assets/Script/LineController.cs
+++ Assets/Script/LineController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.Rendering;
 
 public class LineController : MonoBehaviour
 {
@@ -12,14 +13,17 @@ public class LineController : MonoBehaviour
         string _name;
         string _line;
 
-        string State { get => _state; set => _state = value; }
-        string Name { get => _name; set => _name = value; }
-        string Line { get => _line; set => _line = value; }
+        public string State { get => _state; set => _state = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Line { get => _line; set => _line = value; }
     }
+
+    [Tooltip("導入部"), SerializeField]
+    TextAsset _introduction;
 
     List<Linedata> _linedata = new List<Linedata>();
     /// <summary>現在の行</summary>
-    int _lineNum = default;
+    int _lineNum = 0;
 
     /// <summary>現在の行</summary>
     public int LineNum
@@ -27,23 +31,37 @@ public class LineController : MonoBehaviour
         get { return _lineNum; }
     }
 
-    List<string> _line = new();
-
-    private void Start()
+    private void Awake()
     {
-        string textPath = Application.persistentDataPath + "/introduction.txt";
-        var introductionText = File.ReadAllText(textPath).Split('|');
+        var introductionText = _introduction.text.Split('|');
 
         for (int i = 0; i < introductionText.Length; i++)
         {
             var temp = introductionText[i].Split(',');
             _linedata.Add(new Linedata());
+            
+            // 文字列の仕分け
             foreach(var o in temp)
             {
-                
-            }
+                var tempLine = o.Replace("\n", "");
 
-            
+                if(tempLine.Contains("@state:"))
+                {
+                    _linedata[i].State = tempLine.Replace("@state:", "");
+                }
+                else if (tempLine.Contains("@name:"))
+                {
+                    _linedata[i].Name = tempLine.Replace("@name:", "");
+                }
+                else if (tempLine.Contains("@line:"))
+                {
+                    _linedata[i].Line = tempLine.Replace("@line:", "");
+                }
+                else
+                {
+                    Debug.LogWarning($"「{tempLine}」は指定された書式ではない");
+                }
+            }
         }
 
     }
@@ -52,9 +70,11 @@ public class LineController : MonoBehaviour
     /// 最初に現在の行を出力して、そのあとにlineのインデックスを1増やしている
     /// </summary>
     /// <returns></returns>
-    public string LineUpdate()
+    public string LineUpdate(out string nameUI, out string stateUI)
     {
-        string tmptext = _line[_lineNum];
+        string tmptext = _linedata[_lineNum].Line;
+        nameUI = _linedata[_lineNum].Name;
+        stateUI = _linedata[_lineNum].State;
         _lineNum++;
         return tmptext;
     }
